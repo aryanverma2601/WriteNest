6e5178b75cba565125929c6edcc6a4d47ac1e22e
aryanverma2601 <aryanv380@gmail.com>
2025-09-05 13:11:56 +0530

    Setting up frontend and clerk for authentication

---
+++ client/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+} from "@clerk/clerk-react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <header>
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </header>
+  );
+}
+
+export default App;
